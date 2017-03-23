47157af316f9ab80eb443325d65c79ba138c211f
TeddyMa <shijie.ma@gmail.com>
2017-03-23 23:54:21 +0800

    add apache

--- SetHostsAndStartMonitoring.cmd
+++ SetHostsAndStartMonitoring.cmd
@@ -1,4 +1,4 @@
-powershell -executionpolicy bypass -Command "If ($env:HOSTS) { $hosts = $env:HOSTS.Replace(':', ' ').Replace(',', '\r\n'); $hosts | Set-Content 'C:\Windows\System32\drivers\etc\hosts'; 'Applied hosts: ' + $hosts" }
+powershell -executionpolicy bypass -Command "If ($env:HOSTS) { $hosts = $env:HOSTS.Replace(':', ' ').Replace(',', '`n'); $hosts | Set-Content 'C:\Windows\System32\drivers\etc\hosts'; 'Applied hosts: ' + $hosts" }
 
 powershell -executionpolicy bypass -Command "if ($env:env1) { (Get-Content 'c:\inetpub\wwwroot\iis-demo\web.config').replace('Dev', $env:env1) | Set-Content 'c:\inetpub\wwwroot\iis-demo\web.config' };
 
